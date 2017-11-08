c1dcf0d5f4b34eb2cbe294c7b9d283a49264f62b
StoryCraft <dbsxoal123@naver.com>
2017-11-08 17:10:00 +0900

    remove logs folder

--- logger.js
+++ logger.js
@@ -1,6 +1,4 @@
-let fs = require('fs');
-
-const LOG_FOLDER = 'logs';
+﻿let fs = require('fs');
 
 const WRITER_SETTINGS = {
   flags: 'a',
@@ -49,7 +47,7 @@ class Logger {
 
   cacheLog(log){
     var text = (log.type ? log.type + ' ' : '') + '[' + log.time.toLocaleTimeString() + '] ' + log.text;
-    fs.appendFile(LOG_FOLDER + '/' + this._logFileName,text + '\r\n',WRITER_SETTINGS, (err) => {
+    fs.appendFile(this._logFileName,text + '\r\n',WRITER_SETTINGS, (err) => {
       if (err){
         console.log('로깅중 오류가 발생했습니다.');
         throw err;
